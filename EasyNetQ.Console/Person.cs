6d78359de55053f1e59f0c3992876e76fcfe1c21
TIAGO MARQUES <ads.tiagomarques@gmail.com>
2023-10-28 13:59:53 -0300

    realizando publicacao com a biblioteca EasyNetQ

---
+++ EasyNetQ.Console/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace EasyNetQ.Console
+{
+    public class Person
+    {
+        public Person(string fullName, string document, DateTime birthDate)
+        {
+            fullName = fullName;
+            Document = document;
+            birthDate = birthDate;
+        }
+
+        public string fullName { get; set; }
+        public string Document { get; set; }
+        public DateTime birthDate { get; set; }
+    }
+}
